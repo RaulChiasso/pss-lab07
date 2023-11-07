0f32ed615c755963f1d799bf5d1085eb55260df0
Raul Chiasserini <raul.chiasserini@studio.unibo.it>
2023-11-07 11:59:48 +0100

    Fix FizzBuzz.java for new rules

--- 02-tdd/src/main/java/it/unibo/a/FizzBuzz.java
+++ 02-tdd/src/main/java/it/unibo/a/FizzBuzz.java
@@ -13,24 +13,28 @@ public class FizzBuzz {
     public FizzBuzz() {
     }
 
-    public String calculateString(int index) {
+    public String calculateString(Integer index) {
         String result = ""; 
+        if (index.toString().toCharArray()[0] == 3 || index.toString().toCharArray()[1] == 3) {
+            fizz();
+        }
+
         if (index % 3 != 0 && index % 5 != 0){
             result += index;
         } else if (index % 3 ==0) {
-            result += Fizz();
+            result += fizz();
         } if (index % 5 ==0) {
-            result +=Buzz();
+            result +=buzz();
         }
         result += "\n";
         return result;
     }
 
-    private String Fizz() {
+    private String fizz() {
         return FIZZ;
     }
 
-    private String Buzz() {
+    private String buzz() {
         return BUZZ;
     }
 }
